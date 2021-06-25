90770f4ae1b422da28ec4e4c689f5ba3491c2f53
leangarassino <leandro.garassino@gmail.com>
2021-06-25 07:07:39 -0300

    Endpoint testimonial creation

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var { body } = require('express-validator');
 
 const {Login} = require('./../controllers/auth-controller');
+const {PostTestimonials} = require('./../controllers/testimonials')
+const {validateCreate} = require('./../middlewares/testimonials')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,5 +14,7 @@ router.get('/', function(req, res, next) {
 /*Authentication*/
 router.post('/auth/login', body('email').isEmail(), body('password').isLength({ min: 8 }), Login);
 
+/*Testimonials*/
+router.post('/testimonials', validateCreate , PostTestimonials)
 
 module.exports = router;
