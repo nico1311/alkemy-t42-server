daf5c9a12d54114ae4aec425bbbb25f5ec1938cc
nico1311 <nicolasep1311@outlook.com>
2021-06-25 16:48:07 -0300

    AuthController -> auth-controller, minor changes to controller and routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 
-const AuthController = require('../controllers/AuthController');
+const AuthController = require('../controllers/auth-controller');
+
+router.post('/login',
+  body('email').isEmail(),
+  body('password').isLength({ min: 8 }),
+AuthController.login);
 
 router.post('/register',
   body('first_name').isLength({ min: 1 }),
