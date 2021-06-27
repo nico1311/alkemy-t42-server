f600a94de659d832cd571bd2a4f6be6efd7d1264
zoecrodz <zoerocca@gmail.com>
2021-06-27 17:53:15 -0300

    slides and categories migrations and models, express middleware

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var userRoutes = require("./users")
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use("/user", userRoutes)
 
 module.exports = router;
