f176cb784672fed934678a276fa237f292dde0ec
lggerman <lopez.ggerman@gmail.com>
2021-06-29 05:26:55 -0300

    added comments for documentation

--- routes/publicData.js
+++ routes/publicData.js
@@ -2,18 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  //get to endpoint "/organizations/1/public"
   try {
+    //Json public data
     const publicData = {
-      name: "ONG name example",
+      name: "Somos Más",
       image:
-        "https://cdn-sp.radionacional.com.ar/wp-content/uploads/2017/04/ONG.png", //example logo
-      phone: "1234567890",
-      address: "Adress 1234, CT 0000 example",
+        "https://cdn-sp.radionacional.com.ar/wp-content/uploads/2017/04/ONG.png", //example url logo
+      phone: "1160112988",
+      address: "Address 1234, 0000 example",
       welcomeText: "Welcome text example",
+      //Social links is object with social links in the footer.
+      //Key = Name of social network
+      //Value = Profile Link
       socialLinks: {
-        instagram: "https://www.instagram.com",
-        facebook: "https://www.facebook.com",
-        twitter: "https://www.twitter.com",
+        Instagram: "https://www.instagram.com/SomosMás",
+        Facebook: "https://www.facebook.com/Somos_Más",
       },
     };
     res.status(200).json(publicData);
