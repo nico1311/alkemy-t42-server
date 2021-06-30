28b6eda69e5ed0eae89874960fe39a2dda6c33e9
Dany-Rolon <dany_rolon@outlook.com>
2021-06-30 18:59:01 -0300

    add route to indexRoute

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const entriesRouter = require('./entries')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/news', entriesRouter)
 
 module.exports = router;
