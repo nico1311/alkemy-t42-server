b0cc270ac93ec27259e8de35e1094402f943202a
Dany-Rolon <dany_rolon@outlook.com>
2021-07-01 18:24:16 -0300

    Added route fot get the details from a news

--- controllers/EntriesController.js
+++ controllers/EntriesController.js
@@ -7,7 +7,7 @@ module.exports = {
   /**
    * Get all the entries with type 'news'
    * @param {import('express').Request} req
-   * @param {import('express').Request} res
+   * @param {import('express').Response} res
    * @returns {Promise<void>}
    */
   async getNews(req, res) {
@@ -17,6 +17,20 @@ module.exports = {
         attributes: ['name', 'image', 'createdAt', 'id']
       });
       res.json({ news });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  /**
+   * Get the details for the entry with the specific id
+   * @param {import('express').Request} req
+   * @param {import('express').Response } res
+   */
+  async getNewsDetail(req, res) {
+    try {
+      const newsDetail = await Entry.findByPk(req.params.id);
+      newsDetail === null ? res.status(204).end() : res.json({ newsDetail });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
--- routes/entries.js
+++ routes/entries.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middlewares/verifyToken');
-const checkAdmin = require('../middlewares/checkAdmin');
-const { getNews } = require('../controllers/EntriesController');
+const { getNews, getNewsDetail } = require('../controllers/EntriesController');
 
-router.get('/news', [verifyToken, checkAdmin], getNews);
+router.get('/', verifyToken, getNews);
+router.get('/:id', verifyToken, getNewsDetail);
 
 module.exports = router;
