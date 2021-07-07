064e70e61151a92f021c0f89dd5baef1cbca775e
nico1311 <nicolasep1311@outlook.com>
2021-07-06 21:38:02 -0300

    Added missing field to contact model

--- models/contact.js
+++ models/contact.js
@@ -22,6 +22,7 @@ module.exports = (sequelize, DataTypes) => {
       name: DataTypes.STRING,
       phone: DataTypes.STRING,
       email: DataTypes.STRING,
+      message: DataTypes.STRING,
       deleteAt: DataTypes.DATEONLY
     },
     {
