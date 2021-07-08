3e83bf2e04f211a8c8a1242340ddb28206aaf6fd
Celeste <celestecabrera.2711@gmail.com>
2021-07-07 21:43:13 -0300

    Added checkAdmin middleware

--- routes/entries.js
+++ routes/entries.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middlewares/verifyToken');
+const checkAdmin = require('../middlewares/checkAdmin');
 const { getNews, getNewsDetail, deleteNew } = require('../controllers/EntriesController');
 
 router.get('/', verifyToken, getNews);
 router.get('/:id', verifyToken, getNewsDetail);
-router.delete('/:id', verifyToken,deleteNew );
+router.delete('/:id', verifyToken, checkAdmin, deleteNew );
 
 
 module.exports = router;
