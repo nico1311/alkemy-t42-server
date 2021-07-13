0d5443555eeffeb1478fcd97d2545577b05b6092
lggerman <lopez.ggerman@gmail.com>
2021-07-13 17:02:27 -0300

    created endpoints to delete and obtain only one activity

--- controllers/ActivitiesController.js
+++ controllers/ActivitiesController.js
@@ -1,23 +1,66 @@
-const { activity } = require('../models')
-const Joi = require('Joi')
-const log = require('../utils/logger')
+const { Activity } = require('../models');
+const Joi = require('Joi');
+const log = require('../utils/logger');
 
 module.exports = {
-
-    /**
-     * Get all activities
-     * @param {import('express').Request} req 
-     * @param {import('express').Response} res
-     * @returns {Promise<void>} 
-     */
-    async getAllActivities(req, res) {
-        try {
-            const activities = await activity.findAll()
-            log.info('Seending all the activities')
-            res.status(200).json({activities: activities})
-        } catch (err) {
-            log.err(`Error happened trying to send all the activities. Error: [${err.message}]`)
-            res.status(500).json({Error: err.message})
-        }
+  /**
+   * Get all activities
+   * @param {import('express').Request} req
+   * @param {import('express').Response} res
+   * @returns {Promise<void>}
+   */
+  async getAllActivities(req, res) {
+    try {
+      const activities = await Activity.findAll();
+      log.info('Sending all the activities');
+      res.status(200).json({ activities: activities });
+    } catch (err) {
+      log.error(
+        `Error happened trying to send all the activities. Error: [${err.message}]`
+      );
+      res.status(500).json({ Error: err.message });
     }
-}
+  },
+  /**
+   * Get one acitivity
+   * @param {import('express').Request} req
+   * @param {import('express').Response} res
+   * @returns {Promise<void>}
+   */
+  async getOneActivity(req, res) {
+    try {
+      const activity = await Activity.findOne({ where: { id: req.params.id } });
+      if (activity === null) {
+        log.error('Activity Not Found.');
+        res.sendStatus(404);
+      } else {
+        log.info('Sending one activity');
+        res.status(200).json({ Activity: activity });
+      }
+    } catch (err) {
+      log.error(
+        `Error happened trying to send the activity. Error: [${err.message}]`
+      );
+      res.status(500).json({ Error: err.message });
+    }
+  },
+  /**
+   * Delete one acitivity
+   * @param {import('express').Request} req
+   * @param {import('express').Response} res
+   * @returns {Promise<void>}
+   */
+  async deleteActivity(req, res) {
+    try {
+      const response = await Activity.destroy({ where: { id: req.params.id } });
+      log.info('Deleting one activity');
+      if (response == 1) res.sendStatus(204);
+      else res.sendStatus(404);
+    } catch (err) {
+      log.error(
+        `Error happened trying to delete the activity. Error: [${err.message}]`
+      );
+      res.status(500).json({ Error: err.message });
+    }
+  }
+};
--- routes/activities.js
+++ routes/activities.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const Router = express.Router();
-const { getAllActivities } = require('../controllers/ActivitiesController');
+const router = express.Router();
+const verifyToken = require('../middlewares/verifyToken');
+const checkAdmin = require('./../middlewares/checkAdmin');
+const {
+  getAllActivities,
+  getOneActivity,
+  deleteActivity
+} = require('../controllers/ActivitiesController');
 
-Router.get('/', getAllActivities);
+router.get('/:id', getOneActivity);
+router.get('/', getAllActivities);
+router.delete('/:id', [verifyToken, checkAdmin], deleteActivity);
 
-module.exports = Router
+module.exports = router;
