e287464ce51e3359149c55b5dc68e7d8c5981ddb
Dany-Rolon <dany_rolon@outlook.com>
2021-07-13 16:08:18 -0300

    Fix some stuff

--- controllers/ActivitiesController.js
+++ controllers/ActivitiesController.js
@@ -14,9 +14,10 @@ module.exports = {
         try {
             const activities = await activity.findAll()
             log.info('Seending all the activities')
-            res.status(200).json(activities)
+            res.status(200).json({activities: activities})
         } catch (err) {
             log.err(`Error happened trying to send all the activities. Error: [${err.message}]`)
+            res.status(500).json({Error: err.message})
         }
     }
 }
