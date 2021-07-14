280c819037d29c380b349cb5395a9f6054ce0735
Celeste <celestecabrera.2711@gmail.com>
2021-07-14 09:58:32 -0300

    Created a function to update testimonials

--- controllers/TestimonialsController.js
+++ controllers/TestimonialsController.js
@@ -1,4 +1,4 @@
-const { Testimonials } = require('../models');
+const { Testimony } = require('../models');
 const log = require('../utils/logger')
 
 module.exports = {
@@ -10,12 +10,39 @@ module.exports = {
    */
   async createTestimonial(req, res) {
     try {
-      const response = await Testimonials.create(req.body);
+      const response = await Testimony.create(req.body);
       log.info('Testimonial created');
       res.json(response);
     } catch (err) {
       log.error(`There was an error creating a testimonial. Error [${err.message}]`)
       res.status(500).json({ error: err.message });
     }
+  },
+  async putTestimonial(req, res) {
+    log.info('Editing an testimonial');
+    const id = req.params.id;
+
+    try{
+      const testimonial = await Testimony.findByPk(id);
+
+      if(!testimonial)
+      {
+        log.warn(`Entry with id [${id}]`);
+        return res.status(404).json("Testimonial not found");
+      }
+
+      const operation = await testimonial.update(req.body);
+
+      if(operation)
+      {
+        log.info(`Testimonial with id [${id}] was edited`);
+        return res.status(200).json(testimonial);
+      }
+    } catch(err) {
+      console.log(err);
+      log.error(`Error happened trying to edit a testimonial. Error: [${err.message}]`);
+      return res.status(500).json("Ha ocurrido un error. Por favor, inténtelo nuevamente");
+    }
+    
   }
 };
--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createTestimonial } = require('../controllers/TestimonialsController');
+const { createTestimonial, putTestimonial } = require('../controllers/TestimonialsController');
+const verifyToken = require('../middlewares/verifyToken');
+const checkAdmin = require('../middlewares/checkAdmin');
 const { validateCreate } = require('./../middlewares/testimonials');
 
 router.post('/', validateCreate, createTestimonial);
+router.put('/:id', [verifyToken, checkAdmin], putTestimonial);
 
 module.exports = router;
