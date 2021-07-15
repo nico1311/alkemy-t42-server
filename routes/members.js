154fddbdff8c69cb4db6930a0bdd09be6555282e
leangarassino <leandro.garassino@gmail.com>
2021-07-15 08:48:09 -0300

    endpoint to list members

--- routes/members.js
+++ routes/members.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const Router = express.Router()
-const { createMember } = require('../controllers/MembersController')
+const { createMember, getMember } = require('../controllers/MembersController')
 const verifyToken = require('../middlewares/verifyToken')
 const checkAdmin = require('../middlewares/checkAdmin')
 
 // falta poner los middlewares de seguridad
+Router.get('/', verifyToken, getMember)
 Router.post('/', createMember)
 
 module.exports = Router
