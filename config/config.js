3b1a57cf4464a17fddc72cbc809c3d92551cad4f
Peter Damián Gómez <PeterDamianG@Gmail.com>
2021-07-15 22:29:42 -0300

    config example sqlite db for testing

--- config/config.js
+++ config/config.js
@@ -10,17 +10,15 @@ module.exports = {
     dialect: 'mysql'
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'sqlite',
+    storage: 'database.sqlite3'
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER_PRODUCTION,
+    password: process.env.DB_PASSWORD_PRODUCTION,
+    database: process.env.DB_NAME_PRODUCTION,
+    host: process.env.DB_HOST_PRODUCTION,
+    port: process.env.DB_PORT_PRODUCTION,
     dialect: 'mysql'
   }
 };
