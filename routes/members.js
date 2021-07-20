4dff6dee9bda51c82f3eb2dadf7f12cfcd843ead
Dany-Rolon <dany_rolon@outlook.com>
2021-07-20 13:18:21 -0300

    Created validation middleware for Members

--- routes/members.js
+++ routes/members.js
@@ -3,9 +3,10 @@ const Router = express.Router()
 const { createMember, getAllMembers } = require('../controllers/MembersController')
 const verifyToken = require('../middlewares/verifyToken')
 const checkAdmin = require('../middlewares/checkAdmin')
+const validateMember = require('../validations/MembersValidation')
 
 // falta poner los middlewares de seguridad
 Router.get('/', [verifyToken, checkAdmin], getAllMembers)
-Router.post('/', [verifyToken, checkAdmin], createMember)
+Router.post('/', [verifyToken, checkAdmin, validateMember], createMember)
 
 module.exports = Router
