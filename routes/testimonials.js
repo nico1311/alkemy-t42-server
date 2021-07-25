765d233133a1f0b6563678b0a2585f8309e44293
unknown <cabreraceleste672@gmail.com>
2021-07-25 00:19:01 -0300

    Added route to get just one testimony

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createTestimonial, putTestimonial, getTestimonials } = require('../controllers/TestimonialsController');
+const { createTestimonial, putTestimonial, getTestimonials, getTestimony } = require('../controllers/TestimonialsController');
 const verifyToken = require('../middlewares/verifyToken');
 const checkAdmin = require('../middlewares/checkAdmin');
 const { validateCreate } = require('./../middlewares/testimonials');
 
 router.get('/', verifyToken, getTestimonials);
+router.get('/:id', verifyToken, getTestimony);
 router.post('/', validateCreate, createTestimonial);
 router.put('/:id', [verifyToken, checkAdmin], putTestimonial);
 
