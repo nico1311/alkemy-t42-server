9c1b68e23011d1aaaa8e50fa892f5631bc056ac3
leangarassino <leandro.garassino@gmail.com>
2021-07-27 12:29:27 -0300

    change seed members and activity

--- seeders/20210630192530-create-demo-activity.js
+++ seeders/20210630192530-create-demo-activity.js
@@ -6,27 +6,27 @@ module.exports = {
       'Activities',
       [
         {
-          name: 'Comida solidaria',
+          name: 'Apoyo Escolar',
           image:
-            'https://lh3.googleusercontent.com/pw/AM-JKLXSdlay9RDjfjoGVpwV-lqIU79Q3cUcuRBdIrms4s0O_t5uoXN5UlqBMG9u1f_ST7lX6A37HF0r02rqUmrZJpNvMJl3rOU5JlPdNffTAkZj7rVpDHbaV7DLscluCZVGcj01hl2Cs802105NzQYdLmex1g=w901-h600-no?authuser=0',
-          content: 'Brindar alimentos a los que no tienen',
+            'https://lh3.googleusercontent.com/pw/AM-JKLUcYs5CiGoLNqAFGmIkbnSqZAcmlTeP9SHUgHWlFXsD6JxOTdULKco9aSeyYO_8OEk80lR5ahPw5GJVSo5qfq5rcap8MoUM8mp0REZkbGurkeD7c3Vg1mlDpDadIafICgIiVEDQiBZwPhieyJMIimmE9A=w960-h538-no?authuser=0',
+          content: 'El corazón del área educativa.',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
-          name: 'Recolección',
-          image: 'https://lh3.googleusercontent.com/pw/AM-JKLWzeo1Fh9l0SP1JKODPWgjCxitjdENBQiY8O2Fl0T3rQHerRhRPIRGTeFIrkczFnVTyNyvg6PObHDw62cY73Y5eZG2s4H-5rniyldkNSombxafE27EuX9R4_V-cERX0pLUxjkeazVbF43mJK32Tv-x5_w=w900-h600-no?authuser=0',
-          content: 'Limpieza y recolección de los residuos',
+          name: 'Tutorías',
+          image: 'https://lh3.googleusercontent.com/pw/AM-JKLUtU1QidC-At0QOTCBtp9AClbndFkBicyXTrOiJgXfkomcGjvPAPLYrJgjtb8mXKbg7d2FrkHtUpDk00Xch5R8CLUjYsDmRTkF8Qc7DA7ov6o-l8SloqL1fVB7n9KA7LzgbVFVjHtKJgVFZQl4umvLNqA=w960-h538-no?authuser=0',
+          content: 'Programa destinado a jóvenes',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
-          name: 'Vías verdes',
-          image: 'https://lh3.googleusercontent.com/pw/AM-JKLUXXtNKbtI-C1Kxb3SYB0ms3Ro__MUawTOUKgc4mmxkFjAukameYKzlSZoAeGlXlnsUKOkPHX4LbOslBgNUv2vl2wINorSA5uExbHjWt1_ItaNriSaM_nenY11tytF1jGxDJ4-whzrdI82cPGTMjrZAww=w900-h600-no?authuser=0',
-          content: 'Reforestación de árboles autóctonos',
+          name: 'Taller arte y cuentos',
+          image: 'https://lh3.googleusercontent.com/pw/AM-JKLX4RhamNEIEN5rHZqULmOhWr_IMr23CEvXtjCH5UH_SFG8QzeGHTas1_jjnLfoDv-W_V1o_YMMkR7nJWzInBmoibzR40RXuZp4xNybJcfYCtNBWvnTflVNDY5F5dK7GZMFTdLcIBzRT3Z294LRtY7dSMg=w1067-h600-no?authuser=0',
+          content: 'Taller literario y de manualidades',
           createdAt: new Date(),
           updatedAt: new Date()
-        }
+        },
       ],
       {}
     );
